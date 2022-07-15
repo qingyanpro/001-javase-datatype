f33569a78e73d097102a3642dbb8cbcd327443be
qingyan <qingyan@163.com>
2022-07-15 19:00:23 +0800

    first commited

---
+++ src/main/java/Test01.java
@@ -0,0 +1,14 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.Scanner;
+
+public class Test01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print( i + "*" + j + "=" + i*j +  "\t");
+            }
+            System.out.println();
+        }
+    }
+}
