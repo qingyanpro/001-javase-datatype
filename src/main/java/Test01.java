7d12ddb231ae8474c5ce0b9b6301ab81ce5504cd
清晏 <q17737394887@163.com>
2022-07-16 05:50:27 +0000

    update src/main/java/Test01.java.

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -8,7 +8,7 @@ public class Test01 {
             for (int j = 1; j <= i ; j++) {
                 System.out.print( i + "*" + j + "=" + i*j +  "\t");
             }
-            System.out.println();
+            System.out.println("hello git");
         }
     }
 }
